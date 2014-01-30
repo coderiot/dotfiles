9fe5bad7ab17f18e3ae0e00e13b69d48cdee7f7c
peterrr <peterrr@github>
2014-01-30 17:23:06 +0100

    zsh: fixing colors for vcs_info prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -2,16 +2,16 @@ autoload -U colors && colors
 
 autoload -Uz vcs_info
 
-zstyle ':vcs_info:*' stagedstr '%F{28}●'
-zstyle ':vcs_info:*' unstagedstr '%F{11}●'
+zstyle ':vcs_info:*' stagedstr "%{$fg_bold[green]%}●%{$reset_color%}"
+zstyle ':vcs_info:*' unstagedstr "%{$fg_bold[yellow]%}●%{$reset_color%}"
 zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{11}%r'
 zstyle ':vcs_info:*' enable git svn
 precmd () {
     if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
-    zstyle ':vcs_info:*' formats '[%F{green}%b%c%u%F{cyan}]%f'
+        zstyle ':vcs_info:*' formats '[%F{green}%b%c%u%F{cyan}]%f'
     } else {
-        #zstyle ':vcs_info:*' formats '[%F{green}%b%c%u%F{red}●%F{cyan}]%f'
+       zstyle ':vcs_info:*' formats '[%F{green}%b%c%u%F{red}●%F{cyan}]%f'
     }
 
     vcs_info
