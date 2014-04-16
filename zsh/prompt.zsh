99362ab960b3cec78abffef1bd15b64297b3bc30
peterr <coderiot@zoho.com>
2014-04-16 18:43:57 +0200

    zsh: clear rprompt in pre_cmd

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -8,6 +8,7 @@ zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{11}%r'
 zstyle ':vcs_info:*' enable git svn
 precmd () {
+    RPROMPT=""
     if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
         zstyle ':vcs_info:*' formats '[%F{green}%b%c%u%F{cyan}]%f'
     } else {
